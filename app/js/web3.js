9f85eb6f378933451163a4bf8e3a974cf68a2cb9
Kyler Chin <kylerschin@gmail.com>
2017-10-10 23:10:53 -0700

    basic erc20 interaction template...
    - This will be expanded after GDD to include erc223 standards. For now... I'm short on time and lazy.

--- app/js/web3.js
+++ app/js/web3.js
@@ -5,5 +5,7 @@ if (typeof web3 !== 'undefined') {
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
 
+console.log(web3.version)
+
 var coinbase = web3.eth.coinbase;
 var balance = web3.eth.getBalance(coinbase);
