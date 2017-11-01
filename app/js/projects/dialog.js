30c9bc059b8ef22d92a2a4e3359448ca45e0556f
Kyler Chin <kylerschin@gmail.com>
2017-11-01 10:28:35 -0700

    working fund card

--- app/js/projects/dialog.js
+++ app/js/projects/dialog.js
@@ -43,7 +43,7 @@
     if (projectStatus = "funding") {
       document.querySelector('#amountmyCO2').innerHTML = co2Potential;
       document.querySelector('#co2txtbox1desc').innerHTML = "Annual CO2 Offset";
-      document.querySelector('#ethansStupidButtons').innerHTML = '<div class="mdl-dialog__actions"><button type="button" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" online="fundInfoDialog.close()">Cancel</button><button type="button" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect colorfulEthanFund">Fund</button></div>';
+      document.querySelector('#ethansStupidButtons').innerHTML = '<div class="mdl-dialog__actions"><button type="button" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" online="fundInfoDialog.close();">Cancel</button><button type="button" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect colorfulEthanFund" onclick="fundInfoDialog.close(); fundDialog1.showModal();">Fund</button></div>';
     }
     fundInfoDialog.showModal();
     console.log(projectlat,projectlong);
